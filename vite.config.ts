721b794d43b8d85ea5235ceb56cf04b45688d72c
elliot-brooks <ellitothd@gmail.com>
2026-06-09 22:19:57 +0100

    Update config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
+  base: '/bean-street/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
